36317c52896d8642ae10c3c18774991f4f68bf22
Gianni <giannirosato@proton.me>
2024-02-27 02:36:59 -0500

    refactor: functions as struct methods

--- src/main.zig
+++ src/main.zig
@@ -20,6 +20,38 @@ pub const QoiDesc = struct {
     height: u32 = 0,
     channels: u8 = 0,
     colorspace: u8 = 0,
+
+    fn qoiSetEverything(w: u32, h: u32, ch: u8, c: u8) QoiDesc {
+        return QoiDesc{ .width = w, .height = h, .channels = ch, .colorspace = c };
+    }
+    fn writeQoiHeader(self: QoiDesc, dest: *[14]u8) void {
+        @memcpy(dest[0..4], QOI_MAGIC);
+        std.mem.writeInt(u32, dest[4..8], self.width, .big);
+        std.mem.writeInt(u32, dest[8..12], self.height, .big);
+        dest[12] = self.channels;
+        dest[13] = self.colorspace;
+    }
+    fn qoiEncInit(self: QoiDesc, enc: *QoiEnc, data: [*]u8) !void {
+        for (0..64) |i| {
+            enc.buffer[i].vals.red = 0;
+            enc.buffer[i].vals.green = 0;
+            enc.buffer[i].vals.blue = 0;
+            enc.buffer[i].vals.red = 255;
+        }
+
+        enc.len = self.width * self.height;
+        enc.pad = 0;
+        enc.run = 0;
+        enc.pixel_offset = 0;
+
+        enc.prev_pixel.vals.red = 0;
+        enc.prev_pixel.vals.green = 0;
+        enc.prev_pixel.vals.blue = 0;
+        enc.prev_pixel.vals.alpha = 255;
+
+        enc.data = data;
+        enc.offset = enc.data + 14;
+    }
 };
 
 pub const QoiPixel = extern union {
@@ -45,82 +77,70 @@ pub const QoiEnc = struct {
 
     run: u8,
     pad: u24,
+
+    fn qoiEncRun(self: *QoiEnc) void {
+        const tag: u8 = @intFromEnum(QoiEnum.QOI_OP_RUN) | (self.run - 1);
+        self.run = 0;
+
+        self.offset[0] = tag;
+        self.offset += 1;
+    }
+    fn qoiEncLuma(self: *QoiEnc, green_diff: i8, dr_dg: i8, db_dg: i8) void {
+        const green_diff_biased: u8 = @intCast(green_diff + 32);
+        const dr_dg_biased: u8 = @intCast(dr_dg + 8);
+        const db_dg_biased: u8 = @intCast(db_dg + 8);
+
+        const tags = [2]u8{ @intFromEnum(QoiEnum.QOI_OP_LUMA) | green_diff_biased, dr_dg_biased << 4 | db_dg_biased };
+
+        for (tags, 0..) |tag, i| self.offset[i] = tag;
+        self.offset += tags.len;
+    }
+    fn qoiEncIndex(enc: *QoiEnc, index_pos: u8) void {
+        const tag: u8 = @intFromEnum(QoiEnum.QOI_OP_INDEX) | index_pos;
+        enc.offset[0] = tag;
+        enc.offset += 1;
+    }
+    fn qoiEncFullColor(enc: *QoiEnc, px: QoiPixel, channels: u8) void {
+        var s: u3 = 0;
+        if (channels > 3) s = 5 else s = 4;
+        const tags: [5]u8 = if (channels > 3) .{
+            @intFromEnum(QoiEnum.QOI_OP_RGBA),
+            px.vals.red,
+            px.vals.green,
+            px.vals.blue,
+            px.vals.alpha,
+        } else .{
+            @intFromEnum(QoiEnum.QOI_OP_RGB),
+            px.vals.red,
+            px.vals.green,
+            px.vals.blue,
+            undefined,
+        };
+
+        for (tags[0..s], 0..s) |tag, i| enc.offset[i] = tag;
+        enc.offset += s;
+    }
+    fn qoiEncDifference(enc: *QoiEnc, red_diff: i32, green_diff: i32, blue_diff: i32) void {
+        const green_diff_biased: u8 = @intCast(green_diff + 2);
+        const red_diff_biased: u8 = @intCast(red_diff + 2);
+        const blue_diff_biased: u8 = @intCast(blue_diff + 2);
+
+        const tag: u8 =
+            @intFromEnum(QoiEnum.QOI_OP_DIFF) |
+            red_diff_biased << 4 |
+            green_diff_biased << 2 |
+            blue_diff_biased;
+
+        enc.offset[0] = tag;
+
+        enc.offset += 1;
+    }
 };
 
 fn printHelp() !void {
-    print("Example usage: qoi_enc <filename> <width> <height> <channels> <colorspace> <output>\n", .{});
+    print("Example usage: qoi-zig <filename> <width> <height> <channels> <colorspace> <output>\n", .{});
     print("Channels:\n3: No transparency\n4: Transparency\n\n", .{});
     print("Colorspace:\n0: sRGB with linear alpha\n1: Linear RGB\n", .{});
-}
-
-fn qoiSetDimensions(desc: *QoiDesc, width: u32, height: u32) void {
-    desc.width = width;
-    desc.height = height;
-}
-
-fn qoiSetChannels(desc: *QoiDesc, channels: u8) void {
-    desc.channels = channels;
-}
-
-fn qoiSetColorspace(desc: *QoiDesc, colorspace: u8) void {
-    desc.colorspace = colorspace;
-}
-
-fn writeQoiHeader(desc: *QoiDesc, dest: *[14]u8) void {
-    @memcpy(dest[0..4], QOI_MAGIC);
-    std.mem.writeInt(u32, dest[4..8], desc.width, .big);
-    std.mem.writeInt(u32, dest[8..12], desc.height, .big);
-    dest[12] = desc.channels;
-    dest[13] = desc.colorspace;
-}
-
-pub fn qoiSetPixelRGBA(pixel: *QoiPixel, red: u8, green: u8, blue: u8, alpha: u8) void {
-    pixel.vals.red = red;
-    pixel.vals.green = green;
-    pixel.vals.blue = blue;
-    pixel.vals.alpha = alpha;
-}
-
-pub fn qoiInitializePixel(pixel: *QoiPixel) void {
-    qoiSetPixelRGBA(pixel, 0, 0, 0, 0);
-}
-
-pub fn qoiGetIndexPos(pixel: QoiPixel) u6 {
-    const r = pixel.vals.red;
-    const g = pixel.vals.green;
-    const b = pixel.vals.blue;
-    const a = pixel.vals.alpha;
-    return @truncate(r *% 3 +% g *% 5 +% b *% 7 +% a *% 11);
-}
-
-fn qoiEncInit(desc: *QoiDesc, enc: *QoiEnc, data: [*]u8) bool {
-    for (0..64) |i| {
-        qoiInitializePixel(&enc.buffer[i]);
-    }
-
-    enc.len = desc.width * desc.height;
-    enc.pad = 0;
-    enc.run = 0;
-    enc.pixel_offset = 0;
-
-    qoiSetPixelRGBA(&enc.prev_pixel, 0, 0, 0, 255);
-
-    enc.data = data;
-    enc.offset = enc.data + 14;
-
-    return true;
-}
-
-fn qoiEncDone(enc: *QoiEnc) bool {
-    return enc.pixel_offset >= enc.len;
-}
-
-fn qoiEncRun(enc: *QoiEnc) void {
-    const tag: u8 = @intFromEnum(QoiEnum.QOI_OP_RUN) | (enc.run - 1);
-    enc.run = 0;
-
-    enc.offset[0] = tag;
-    enc.offset += 1;
 }
 
 fn qoiComparePixel(pixel1: QoiPixel, pixel2: QoiPixel, channels: u8) bool {
@@ -141,70 +161,6 @@ fn qoiComparePixel(pixel1: QoiPixel, pixel2: QoiPixel, channels: u8) bool {
     return p1.concatenated_pixel_values == p2.concatenated_pixel_values;
 }
 
-fn qoiEncIndex(enc: *QoiEnc, index_pos: u8) void {
-    const tag: u8 = @intFromEnum(QoiEnum.QOI_OP_INDEX) | index_pos;
-    enc.offset[0] = tag;
-    enc.offset += 1;
-}
-
-fn qoiEncRGB(enc: *QoiEnc, px: QoiPixel) void {
-    const tags = [4]u8{
-        @intFromEnum(QoiEnum.QOI_OP_RGB),
-        px.vals.red,
-        px.vals.green,
-        px.vals.blue,
-    };
-
-    for (tags, 0..) |tag, i| {
-        enc.offset[i] = tag;
-    }
-    enc.offset += tags.len;
-}
-
-fn qoiEncRGBA(enc: *QoiEnc, px: QoiPixel) void {
-    const tags = [5]u8{
-        @intFromEnum(QoiEnum.QOI_OP_RGBA),
-        px.vals.red,
-        px.vals.green,
-        px.vals.blue,
-        px.vals.alpha,
-    };
-
-    for (tags, 0..) |tag, i| {
-        enc.offset[i] = tag;
-    }
-    enc.offset += tags.len;
-}
-
-fn qoiEncDifference(enc: *QoiEnc, red_diff: i32, green_diff: i32, blue_diff: i32) void {
-    const green_diff_biased: u8 = @intCast(green_diff + 2);
-    const red_diff_biased: u8 = @intCast(red_diff + 2);
-    const blue_diff_biased: u8 = @intCast(blue_diff + 2);
-
-    const tag: u8 =
-        @intFromEnum(QoiEnum.QOI_OP_DIFF) |
-        red_diff_biased << 4 |
-        green_diff_biased << 2 |
-        blue_diff_biased;
-
-    enc.offset[0] = tag;
-
-    enc.offset += 1;
-}
-
-fn qoiEncLuma(enc: *QoiEnc, green_diff: i8, dr_dg: i8, db_dg: i8) void {
-    const green_diff_biased: u8 = @intCast(green_diff + 32);
-    const dr_dg_biased: u8 = @intCast(dr_dg + 8);
-    const db_dg_biased: u8 = @intCast(db_dg + 8);
-
-    const tags = [2]u8{ @intFromEnum(QoiEnum.QOI_OP_LUMA) | green_diff_biased, dr_dg_biased << 4 | db_dg_biased };
-
-    for (tags, 0..) |tag, i| {
-        enc.offset[i] = tag;
-    }
-    enc.offset += tags.len;
-}
-
 fn qoiEncodeChunk(desc: *QoiDesc, enc: *QoiEnc, qoi_pixel_bytes: [*]u8) void {
     var cur_pixel: QoiPixel = undefined;
 
@@ -215,24 +171,24 @@ fn qoiEncodeChunk(desc: *QoiDesc, enc: *QoiEnc, qoi_pixel_bytes: [*]u8) void {
         @memcpy(&cur_pixel.channels, qoi_pixel_bytes[0..4]);
     }
 
-    const index_pos = qoiGetIndexPos(cur_pixel);
+    const index_pos: u6 = @truncate(cur_pixel.vals.red *% 3 +% cur_pixel.vals.green *% 5 +% cur_pixel.vals.blue *% 7 +% cur_pixel.vals.alpha *% 11);
 
     if (qoiComparePixel(cur_pixel, enc.prev_pixel, desc.channels)) {
         enc.run += 1;
         if (enc.run >= 62 or enc.pixel_offset >= enc.len) {
-            qoiEncRun(enc);
+            enc.qoiEncRun();
         }
     } else {
         if (enc.run > 0) {
-            qoiEncRun(enc);
+            enc.qoiEncRun();
         }
         if (qoiComparePixel(enc.buffer[index_pos], cur_pixel, 4)) {
-            qoiEncIndex(enc, index_pos);
+            enc.qoiEncIndex(index_pos);
         } else {
             enc.buffer[index_pos] = cur_pixel;
 
             if (desc.channels > 3 and cur_pixel.vals.alpha != enc.prev_pixel.vals.alpha) {
-                qoiEncRGBA(enc, cur_pixel);
+                enc.qoiEncFullColor(cur_pixel, desc.channels);
             } else {
                 const red_diff: i32 = @as(i32, cur_pixel.vals.red) - @as(i32, enc.prev_pixel.vals.red);
                 const green_diff: i32 = @as(i32, cur_pixel.vals.green) - @as(i32, enc.prev_pixel.vals.green);
@@ -245,14 +201,14 @@ fn qoiEncodeChunk(desc: *QoiDesc, enc: *QoiEnc, qoi_pixel_bytes: [*]u8) void {
                     green_diff >= -2 and green_diff <= 1 and
                     blue_diff >= -2 and blue_diff <= 1)
                 {
-                    qoiEncDifference(enc, @intCast(red_diff), @intCast(green_diff), @intCast(blue_diff));
+                    enc.qoiEncDifference(@intCast(red_diff), @intCast(green_diff), @intCast(blue_diff));
                 } else if (dr_dg >= -8 and dr_dg <= 7 and
                     green_diff >= -32 and green_diff <= 31 and
                     db_dg >= -8 and db_dg <= 7)
                 {
-                    qoiEncLuma(enc, @intCast(green_diff), @intCast(dr_dg), @intCast(db_dg));
+                    enc.qoiEncLuma(@intCast(green_diff), @intCast(dr_dg), @intCast(db_dg));
                 } else {
-                    qoiEncRGB(enc, cur_pixel);
+                    enc.qoiEncFullColor(cur_pixel, desc.channels);
                 }
             }
         }
@@ -261,7 +217,7 @@ fn qoiEncodeChunk(desc: *QoiDesc, enc: *QoiEnc, qoi_pixel_bytes: [*]u8) void {
     enc.prev_pixel = cur_pixel;
     enc.pixel_offset += 1;
 
-    if (qoiEncDone(enc)) {
+    if (enc.pixel_offset >= enc.len) {
         @memcpy(enc.offset[0..QOI_PADDING.len], QOI_PADDING);
         enc.offset += QOI_PADDING.len;
     }
@@ -305,11 +261,7 @@ pub fn main() !void {
         print("{d} bytes are required for {s}. Your file is too small at {d} bytes.\n", .{ image_size, args[1], bytes_read.len });
     }
 
-    var desc: QoiDesc = .{};
-
-    _ = qoiSetDimensions(&desc, width, height);
-    _ = qoiSetChannels(&desc, channels);
-    _ = qoiSetColorspace(&desc, colorspace);
+    var desc = QoiDesc.qoiSetEverything(width, height, channels, colorspace);
 
     const qoi_file_size = @as(usize, desc.width) * @as(usize, desc.height) * (@as(usize, desc.channels) + 1) + 14 + 8 + @sizeOf(usize);
     var qoi_file = try allocator.alloc(u8, qoi_file_size);
@@ -317,14 +269,14 @@ pub fn main() !void {
 
     print("Writing {s} ...\n", .{args[6]});
 
-    _ = writeQoiHeader(&desc, qoi_file[0..14]);
+    desc.writeQoiHeader(qoi_file[0..14]);
 
     var pixel_seek: [*]u8 = bytes_read.ptr;
     var enc: QoiEnc = undefined;
 
-    _ = qoiEncInit(&desc, &enc, qoi_file.ptr);
+    try desc.qoiEncInit(&enc, qoi_file.ptr);
 
-    while (!qoiEncDone(&enc)) {
+    while (!(enc.pixel_offset >= enc.len)) {
         qoiEncodeChunk(&desc, &enc, pixel_seek);
         pixel_seek += desc.channels;
     }
