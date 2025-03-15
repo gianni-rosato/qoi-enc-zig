2373dfd8f884cd0cbb90050c17de7fff8e3d62e7
Gianni <giannirosato@proton.me>
2025-03-15 15:03:47 -0400

    refactor: remove build version error

--- build.zig
+++ build.zig
@@ -1,30 +1,6 @@
 const std = @import("std");
 
-pub const min_zig_version = std.SemanticVersion{ .major = 0, .minor = 13, .patch = 0 };
-
 pub fn build(b: *std.Build) !void {
-    var installed_ver = @import("builtin").zig_version;
-    installed_ver.build = null;
-
-    if (installed_ver.order(min_zig_version) == .lt) {
-        std.log.err("\n" ++
-            \\---------------------------------------------------------------------------
-            \\
-            \\error: Installed Zig compiler version is too old.
-            \\
-        , .{});
-        std.log.err("Minimum required version: \x1b[32m{any}\x1b[0m", .{min_zig_version});
-        std.log.err("Installed version: \x1b[31m{any}\x1b[0m\n", .{installed_ver});
-        std.log.err(
-            \\Please install a newer version & try again.
-            \\Latest version can be found here: https://ziglang.org/download/
-            \\
-            \\---------------------------------------------------------------------------
-            \\
-        , .{});
-        return error.ZigTooOld;
-    }
-
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
